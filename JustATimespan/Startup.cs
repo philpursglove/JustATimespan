7128deddb045fab0c54f4db017fb632b33b1764d
Phil Pursglove <phil@philippursglove.com>
2016-10-05 13:50:44 +0100

    Startup class for SignalR

---
+++ JustATimespan/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+namespace JustATimespan
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
