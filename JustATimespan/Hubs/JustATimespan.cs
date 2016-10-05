e03120ee93dd56584f32d22200edbc1b19d95bda
Phil Pursglove <phil@philippursglove.com>
2016-10-05 16:23:36 +0100

    SIgnalR hub added

---
+++ JustATimespan/Hubs/JustATimespan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace JustATimespan.Hubs
+{
+    public class JustATimespan : Hub
+    {
+        private static bool _started;
+
+        public void Buzz(string name)
+        {
+            Clients.All.sendMessage("STOP");
+            Clients.All.sendMessage(name + " buzzed!");
+        }
+
+        public void Start()
+        {
+            _started = true;
+
+            Clients.All.sendMessage("START");
+        }
+
+        public void TimeUp()
+        {
+            _started = false;
+
+            Clients.All.sendMessage("TIMEUP");
+        }
+
+    }
+}
